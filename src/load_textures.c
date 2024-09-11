39d59a20d970d455ed889f5bdc310d1a337d205e
NafisaKaruri <nafeesahassan016@gmail.com>
2024-09-10 19:22:28 -0700

    fixed load_textures's betty

--- src/load_textures.c
+++ src/load_textures.c
@@ -1,10 +1,17 @@
 #include "../headers/headers.h"
 
-void loadTextures(State *state, SDL_Texture **wallTexture) 
+/**
+ * loadTextures - Loads texture to the walls.
+ *
+ * @state: The state of the game.
+ * @wallTexture: The wall texture.
+ */
+void loadTextures(State *state, SDL_Texture **wallTexture)
 {
-    SDL_Surface* surface = IMG_Load("textures/wall_bricks_old_1024.png");
-    ASSERT(surface, "Failed to load texture: %s\n", IMG_GetError());
-    *wallTexture = SDL_CreateTextureFromSurface(state->renderer, surface);
-    SDL_FreeSurface(surface);
-    ASSERT(*wallTexture, "Failed to create texture: %s\n", SDL_GetError());
+	SDL_Surface *surface = IMG_Load("textures/wall_bricks_old_1024.png");
+
+	ASSERT(surface, "Failed to load texture: %s\n", IMG_GetError());
+	*wallTexture = SDL_CreateTextureFromSurface(state->renderer, surface);
+	SDL_FreeSurface(surface);
+	ASSERT(*wallTexture, "Failed to create texture: %s\n", SDL_GetError());
 }
