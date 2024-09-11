8a81aaf7f2b79a299f43f3f26985f9fc034e01d4
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-09-11 18:22:11 +0300

    edited the load textures funtion to be able to accept any texture from a particular path

--- src/load_textures.c
+++ src/load_textures.c
@@ -1,10 +1,18 @@
 #include "../headers/headers.h"
 
-void loadTextures(State *state, SDL_Texture **texture, const char *path) 
+/**
+ * loadTextures - Creates a texture from a surface.
+ *
+ * @state: The variable containing the window and renderer
+ * @texture: The variable that the texture will be loaded into
+ * @path: the path of the image from which the texture will be creatd
+ */
+void loadTextures(State *state, SDL_Texture **texture, const char *path)
 {
-    SDL_Surface* surface = IMG_Load(path);
-    ASSERT(surface, "Failed to load texture: %s\n", IMG_GetError());
-    *texture = SDL_CreateTextureFromSurface(state->renderer, surface);
-    SDL_FreeSurface(surface);
-    ASSERT(*texture, "Failed to create texture: %s\n", SDL_GetError());
+	SDL_Surface *surface = IMG_Load(path);
+
+	ASSERT(surface, "Failed to load texture: %s\n", IMG_GetError());
+	*texture = SDL_CreateTextureFromSurface(state->renderer, surface);
+	SDL_FreeSurface(surface);
+	ASSERT(*texture, "Failed to create texture: %s\n", SDL_GetError());
 }
