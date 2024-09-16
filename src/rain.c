1d496dc711e9b57f66fbe0802b1b582e604a0fd2
NafisaKaruri <nafeesahassan016@gmail.com>
2024-09-16 12:43:40 +0200

    modified the renderRain func

--- src/rain.c
+++ src/rain.c
@@ -7,6 +7,9 @@
  */
 void renderRain(State *state)
 {
+	/* Set the drawing color for raindrops to transparent light blue */
+	SDL_SetRenderDrawColor(state->renderer, 173, 216, 230, 150);
+
 	/* The loop is the number of drops in the screen */
 	for (int i = 0; i < 100; i++)
 	{
@@ -16,10 +19,10 @@ void renderRain(State *state)
 		/* Random length for the raindrop */
 		int len = rand() % 10 + 5;
 
-		/* Raindrop color is transparent light blue */
-		SDL_SetRenderDrawColor(state->renderer, 173, 216, 230, 150);
-
 		/* Draw the raindrops */
 		SDL_RenderDrawLine(state->renderer, x, y, x, y + len);
 	}
+
+	/* Reset the color to black to avoid affecting other render calls */
+	SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
 }
